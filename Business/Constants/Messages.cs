e67e4572f14e055b04208ba875c45720eed08d4c
HÜSEYİN ALTINOK <huseyin.altinok@std.antalya.edu.tr>
2021-10-28 01:18:24 +0300

    Code Refactoring / Business Katmanında Business Rule Yöntemi

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,9 +7,12 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string ProductAdded = "Ürün Eklendi"; 
+        public static string ProductAdded = "Ürün Eklendi";
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductsListed = "Ürünler Listelendi";
+        public static string ProductCountofCategoryError = "Bir Kategoride en fazla 10 ürün olabilir !!";
+        public static string ProductNameAlreadyExists = "Aynı İsimde Ürün Eklenemez !!";
+        public static string CategoryLimitExceded = "Kategori Limiti Aşıldı!!";
     }
 }
