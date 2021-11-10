78821657d1327169ac78eb754ecffd2b45333bd1
HÜSEYİN ALTINOK <huseyin.altinok@std.antalya.edu.tr>
2021-11-10 22:51:03 +0300

    JWT - Yetkilendirme İşlemi Eklendi.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,13 @@ namespace Business.Constants
         public static string ProductCountofCategoryError = "Bir Kategoride en fazla 10 ürün olabilir !!";
         public static string ProductNameAlreadyExists = "Aynı İsimde Ürün Eklenemez !!";
         public static string CategoryLimitExceded = "Kategori Limiti Aşıldı!!";
+        public static string AuthorizationDenied = "Yetkiniz Yok...";
+        public static string UserAdded = "Kullanıcı Eklendi.";
+        public static string UserRegistered = "Kayıt Oldu.";
+        public static string UserNotFound = "Kullanıcı Bulunamadı.";
+        public static string PasswordError = "Parola Hatası.";
+        public static string SuccessfulLogin = "Başarılı Giriş";
+        public static string UserAlreadyExists = "Kullanıcı Mevcut";
+        public static string AccessTokenCreated = "Token Oluşturuldu.";
     }
 }
