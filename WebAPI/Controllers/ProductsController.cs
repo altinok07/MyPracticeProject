bf17ec4ea0fd3aa42b970784c3c45a85e6b5ed8a
HÜSEYİN ALTINOK <huseyin.altinok@std.antalya.edu.tr>
2021-12-12 15:54:39 +0300

    Category Controller Eklendi

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -25,6 +26,8 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+
+            Thread.Sleep(3000);
             var result = _productService.GetAll();
             if (result.Success)
             {
@@ -42,8 +45,22 @@ namespace WebAPI.Controllers
                 return Ok(result);
             }
             return BadRequest(result);
-
         }
+
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+         
+
+
 
         [HttpPost("add")]
         public IActionResult Add(Product product)
